a4c550518b2aa590eb184fe1be4dca1a245e0f91
Yuri148 <yurisousa.carneiro2006@gmail.com>
2023-08-09 20:11:56 -0300

    Criando calculo de média das notas

---
+++ exemplo03.php
@@ -0,0 +1,11 @@
+<?php 
+
+	echo "Criando mais um projetin <br>";
+
+	$nota1 = 6.5;
+	$nota2 = 8.5;
+	$trabalho = 9.5;
+	$media = ($nota2 + $nota1 + $trabalho)/3;
+
+	echo "Sua média é " . $media;
+?>
